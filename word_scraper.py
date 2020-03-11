110c485d3368d875c72b2db7c5820e8f83d419b6
Vidak Kazic <vidakdk@gmail.com>
2020-03-11 12:01:20 +0100

    make scraper more robust

--- word_scraper.py
+++ word_scraper.py
@@ -2,8 +2,13 @@
 
 import re as _re
 
-import requests as _requests
 import bs4 as _bs4
+import requests as _requests
+#import xlsxwriter
+# import numpy as np
+import pandas
+import xlwt
+from xlwt import Workbook
 
 
 BLACKLISTED_ELEMENTS = [
@@ -19,8 +24,14 @@ BLACKLISTED_ELEMENTS = [
 ]
 
 BLACKLISTED_WORDS = [
-    'class', 'div', 'label', 'fieldset', 'banner', 'popup'
+    'class', 'div', 'label', 'fieldset', 'banner', 'popup', 'and', 'our', 'we', 'is', 'to', 'about','in',
+    'the','at','a','of','s','on','can','for','from','that','how','with','why','out','as','must','are','gmbh','co',
+    'if','ie','endif','gte','jquery','en','script','fr','has','this','more','9','you','us','all','end','noscript',
+    'set','log','be','de','javascript'
 ]
+
+TIMEOUT_IN_SECONDS = 10
+MAX_WORD_LENGTH = 50  # how many characters can one word be long at most
 
 
 def _scrape_website(url: str) -> list:
@@ -35,9 +46,15 @@ def _scrape_website(url: str) -> list:
             (<word>, <number of occurrences>),
             sorted by number of occurrences descending.
     """
-    response = _requests.get(url)
+    try:
+        response = _requests.get(url, timeout=TIMEOUT_IN_SECONDS)
+    except Exception as e:
+        print(f"Failed HTTP request, reason: {e}")
+        return []
+
     if not response.ok:
-        raise RuntimeError(f"Failed HTTP request, status code: {response.status_code}")
+        print(f"Failed HTTP request, status code: {response.status_code}")
+        return []
 
     soup = _bs4.BeautifulSoup(response.content, 'html.parser')
     text = soup.find_all(text=True)
@@ -46,20 +63,47 @@ def _scrape_website(url: str) -> list:
     for segment in text:  # iterate over segments
         if segment.parent.name not in BLACKLISTED_ELEMENTS:  # ignore random html garbage
             for word in _re.split(r'\W', segment.lower()):  # split into words
-                if word and word not in BLACKLISTED_WORDS:  # ignore likely html words
+                if word and word not in BLACKLISTED_WORDS and len(word) <= MAX_WORD_LENGTH:  # ignore likely html words
                     words[word] = 1 if word not in words else words[word] + 1
 
-    # This is a fancy one-line sorting expression, it could be done in a more
-    # readable way, but this is shorter :)
+    # This is a fancy one-line sorting expression, it could be done in a more readable way, but this is shorter :)
     sorted_words = [(k, words[k]) for k in
                     sorted(words.keys(), key=lambda k: words[k], reverse=True)]
 
     return sorted_words
 
 
-if __name__ == '__main__':
-    # url = "https://www.lemo.com/en/products/low-voltage-connector"
-    # url = "https://www.ebay.com/b/Lemo-Connectors/bn_7024950655"
-    url = "https://www.peigenesis.com/en/lemo/lemo-l-series-connector.html"
+input_filename = 'Links2.xlsx'
+df = pandas.read_excel(input_filename)
+
+#with xlsxwriter.Workbook('test.xlsx') as workbook:
+#    worksheet = workbook.add_worksheet()
+
+ #   for row_num, data in enumerate(sorted_words):
+  #      worksheet.write_row(row_num, 0, data)
+
+
+# Workbook is created
+wb = Workbook()
+sheet1 = wb.add_sheet('Sheet 1')
+
+# ovo nam treba da bi newline radio
+style = xlwt.XFStyle()
+style.alignment.wrap = 1
+
+cnt = 0
+for i, row in df.iterrows():
+    url = row['SUBLINK']
     sorted_words = _scrape_website(url)
-    print("\n".join((f"{word}: {count}" for word, count in sorted_words[:20])))
+    words_str = "\n".join((f"{word}: {count}" for word, count in sorted_words))  # napravi string od liste
+    sheet1.write(i, 0, url)
+    sheet1.write(i, 1, words_str, style)
+    cnt += 1
+
+
+wb.save('final.xls')
+
+# below exportation to a txt file
+#with open('test.txt', 'w+') as datafile_id:
+#here you open the ascii file
+#    np.savetxt(datafile_id, sorted_words, fmt=['%s','%s'])
